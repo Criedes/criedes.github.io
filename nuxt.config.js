1665450ffdebced3d468c408b9feff1f5bfc353a
Rattanachat <rattanachat.rs@gmail.com>
2021-07-10 01:27:46 +0700

    add seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import { createSEOMeta } from "./utils/seo";
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -15,7 +17,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      ...createSEOMeta({
+        title: "Rattanachat's Portfolio",
+        description: "Get to know all about Naruto and its characters in tiny bits of info.",
+        image: "@/assets/images/cimb.png",
+        url: "https://rattanachat.com",
+      }),
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
