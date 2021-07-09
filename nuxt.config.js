0b1f85ce5750efbaabf2a2f762f74f0ebbd85db4
Rattanachat <rattanachat.rs@gmail.com>
2021-07-09 08:19:28 +0700

    change base route as a repository name

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/rattanachat-portfolio/'
+    base: '/criedes.github.io/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
