ccdf1a64437687308284678733f57e1d2b7d4269
Rattanachat <rattanachat.rs@gmail.com>
2021-07-10 17:35:23 +0700

    fix seo issue

--- pages/index.vue
+++ pages/index.vue
@@ -19,15 +19,15 @@
 </template>
 
 <script>
-import ogImage from "@/assets/images/cimb.png";
+// import ogImage from "@/assets/images/cimb.png";
 export default {
-  head() {
-    return this.$headUtils({
-      lang: "en",
-      title: "Home - Rattanachat's Portfolio",
-      description: "Get to know all about me, my skills and experiences",
-      image: ogImage,
-    });
-  },
+  // head() {
+  //   return this.$headUtils({
+  //     lang: "en",
+  //     title: "Home - Rattanachat's Portfolio",
+  //     description: "Get to know all about me, my skills and experiences",
+  //     image: ogImage,
+  //   });
+  // },
 };
 </script>
